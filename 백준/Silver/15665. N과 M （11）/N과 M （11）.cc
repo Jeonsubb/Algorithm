f6b44b684db03f410ca809ae8665034e8b182de4
Jeonsubb <143583100+Jeonsubb@users.noreply.github.com>
2024-11-19 17:25:58 +0900

    [Silver II] Title: N과 M (11), Time: 124 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int arr[10];
+int n, m;
+void func(int k, vector<int>vec) {
+	if (k == m) {
+		for (int i = 0; i < m; i++)
+			cout << arr[i] << ' ';
+		cout << '\n';
+		return;
+
+	}
+	int last = -1;
+	for (int i = 0; i < n; i++) {
+		if (vec[i] != last) {
+			arr[k] = vec[i];
+			last = vec[i];
+			func(k + 1, vec);
+		}
+	}
+}
+
+
+int main() {
+	cin.tie(0);
+	ios::sync_with_stdio(false);
+	cin >> n >> m;
+	vector<int>vec(n);
+	for (int i = 0; i < n; i++) cin >> vec[i];
+	sort(vec.begin(), vec.end());
+	
+	func(0, vec);
+}
