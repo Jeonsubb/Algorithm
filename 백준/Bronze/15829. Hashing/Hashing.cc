b4ce37fdf423b2cbe3bb64191d74b094a3622748
Jeonsubb <143583100+Jeonsubb@users.noreply.github.com>
2025-09-26 18:04:26 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cc
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -4,20 +4,32 @@ using namespace std;
 
 int main() 
 {
-	int l;
-	cin >> l;
-	int res = 0;
+    int l;
+    cin >> l;
 
-	for (int i = 0; i < l; i++) 
-	{
-		char c;
-		cin >> c;
-		int n = c - 'a' + 1;
-		int tmp= (n * pow(31, i));
-		res += tmp % 1234567891;
+    long long res = 0;
+    long long r = 1; // 31의 거듭제곱을 저장할 변수 (초기값 31^0 = 1)
+    long long M = 1234567891;
 
-	}
+    for (int i = 0; i < l; i++)
+    {
+        char c;
+        cin >> c;
+        int n = c - 'a' + 1;
 
-	cout << res;
+        // n * (31^i) 항을 계산하고 바로 모듈러 연산
+        // (n * r)이 long long 범위를 넘을 수 있으므로 각각 long long으로 변환
+        long long term = (static_cast<long long>(n) * r) % M;
+
+        // 결과에 더하고 다시 모듈러 연산
+        res = (res + term) % M;
+
+        // 다음 루프에서 사용할 31의 거듭제곱 값을 갱신
+        r = (r * 31) % M;
+    }
+
+    cout << res;
+
+    return 0;
 
 }
